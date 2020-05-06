7cfba85c10028152ac8b372bb1b244a8f4ca6570
Winrich Sy <winrichsy@gmail.com>
2020-05-05 17:15:13 -0700

    added SQL leetcode solutions

---
+++ SQL/CombineTwoTables.sql
@@ -0,0 +1,7 @@
+--Combine Two Tables
+--https://leetcode.com/problems/combine-two-tables/
+
+SELECT Person.FirstName, Person.LastName, Address.City, Address.State
+FROM Person
+LEFT JOIN Address
+ON Person.PersonId = Address.PersonId;
